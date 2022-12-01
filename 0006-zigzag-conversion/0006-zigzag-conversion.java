34fc7c93860b7120a113b22eca0ace715574afc5
mdumar99 <76191234+mdumar99@users.noreply.github.com>
2022-12-01 20:07:41 +0800

    Time: 114 ms (9.42%), Space: 73.5 MB (5.16%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1) return s;
+        char[][] arr = new char[numRows][s.length()];
+        int x=0, y=0;
+        boolean latch = false;
+        for(int i=0; i<s.length(); i++){
+            arr[x][y] = s.charAt(i);
+            if(latch == false){
+                if(x+1<numRows){
+                    x++;
+                }else{
+                    if(x-1!=0) latch = true;
+                    x--;
+                    y++;
+                    continue;
+            }   
+        }
+            if(latch==true){
+                if (x-1!=0){
+                    x--;
+                    y++;
+                }else{
+                    x--;
+                    y++;
+                    latch = false;
+                }
+            }
+        }
+        
+        String res = "";
+        
+        for(int i=0; i<numRows; i++){
+            for(int j=0; j<s.length(); j++){
+                if(arr[i][j]!='\0'){
+                    res = res + arr[i][j];
+                }
+            }
+        }
+        
+        return res;
+    }
+}
