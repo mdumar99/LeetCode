46231d727bf38b9b0c8eb3a63b5c9c71793992f0
mdumar99 <76191234+mdumar99@users.noreply.github.com>
2023-01-03 23:47:42 +0800

    Time: 60 ms (62.53%), Space: 12.3 MB (30.23%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -3,12 +3,14 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int count = 0;
         for(int i=0; i<strs[0].length(); i++){
-            vector<char> cl;
-            for(int j=0; j<strs.size(); j++){
-                cl.push_back(strs[j][i]);
-            }
-            if(!std::is_sorted(cl.begin(), cl.end())){
-                count++;
+            char prev = strs[0][i];
+            for(int j=1; j<strs.size(); j++){
+                if(strs[j][i]<prev){
+                    count++;
+                    break;
+                }
+                
+                prev = strs[j][i];
             }
         }
         
